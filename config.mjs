d9799d670103adaa94f1b61ae0e02c9982b4414b
Mugiwara <37186888+ciss84@users.noreply.github.com>
2023-12-25 14:02:04 +0100

    Add files via upload

--- config.mjs
+++ config.mjs
@@ -84,13 +84,14 @@ function DetectFirmwareVersion()
     {
         return ps4_9_00;
     }
-    
+
     UA = navigator.userAgent.substring(navigator.userAgent.indexOf('5.0 (') + 19, navigator.userAgent.indexOf(') Apple')).replace("PlayStation 5/","");
     
     if (UA == "5.00" || UA == "5.10" || UA == "5.50")
     {
         return ps5_5_00;
     }
+
 }
 
 export let target = DetectFirmwareVersion();
